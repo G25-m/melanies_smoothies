6e6e8e2ffa2c8fe63670815091a188ad6dca8532
G25-m <mwong@north.com>
2025-10-30 18:50:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)    
         
-        st.success('Your Smoothie is ordered!', icon="✅")
+st.success('Your Smoothie is ordered!', icon="✅")
